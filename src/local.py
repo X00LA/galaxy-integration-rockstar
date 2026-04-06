c8217df256ae20a85ecd349ac7db13956ca816cd
X00LA <xoolalp@gmail.com>
2026-04-06 10:01:22 +0200

    Revert back to 0.5.13
    Removed unnecessary concatenation of launch parameters for subprocess call.

--- src/local.py
+++ src/local.py
@@ -107,12 +107,7 @@ class LocalClient:
             return
         game_path = f"{path}\\{games_cache[title_id]['launchEXE']}"
         log.debug(f"ROCKSTAR_LAUNCH_REQUEST: Requesting to launch {game_path}...")
-        
-        launch_params = "-launchTitleInFolder"
-        if "cmdLineArgs" in games_cache[title_id]:
-            launch_params += " " + games_cache[title_id]["cmdLineArgs"]
-
-        subprocess.Popen([game_path, launch_params, path, "@commandline.txt"], stdout=subprocess.DEVNULL,
+        subprocess.Popen([game_path, "-launchTitleInFolder", path, "@commandline.txt"], stdout=subprocess.DEVNULL,
                          stderr=subprocess.DEVNULL, shell=False)
         launcher_pid = None
         retries = 120
